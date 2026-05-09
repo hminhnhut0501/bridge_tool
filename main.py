0cb93b5d2f9c0b2e6833364effb845c0106e4385
Huynh Minh Nhut <hminhnhut@Minsuns-MacBook-Pro.local>
2026-05-09 21:07:54 +0700

    Thêm Web server giả để lấy gói Free Render

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import threading
+import http.server
+import socketserver
+
+# Tạo server giả để Render không báo lỗi Port
+def run_dummy_server():
+    PORT = 8080
+    handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print(f"📡 Dummy server started at port {PORT}")
+        httpd.serve_forever()
+
+# Chạy server ở luồng riêng
+threading.Thread(target=run_dummy_server, daemon=True).start()
+
 import asyncio
 from bot_instance import bot, dp, set_commands
 from bot_handlers import router
